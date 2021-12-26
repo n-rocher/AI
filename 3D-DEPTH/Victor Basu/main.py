7619df740debc2b8bd44c24a148627429113dac5
Nathan ROCHER <nathan.rocher@etu.univ-nantes.fr>
2021-12-26 13:29:40 +0100

    3D DEPTH

--- 3D-DEPTH/Victor Basu/main.py
+++ 3D-DEPTH/Victor Basu/main.py
@@ -9,8 +9,6 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-
-
 from datagenerator import DataGenerator 
 from model import DepthEstimationModel 
 
@@ -19,7 +17,7 @@ if __name__ == "__main__":
 
     tf.random.set_seed(123)
  
-    annotation_folder = r"F:\\DIODE\\"
+    annotation_folder = r"./DIODE/"
     if not os.path.exists(annotation_folder):
         annotation_zip = tf.keras.utils.get_file(
             "val.tar.gz",
@@ -29,7 +27,7 @@ if __name__ == "__main__":
         )
 
 
-    path = annotation_folder + "val/indoors"
+    path = annotation_folder + "train/outdoor"
 
     filelist = []
 
@@ -43,10 +41,10 @@ if __name__ == "__main__":
         "depth": [x for x in filelist if x.endswith("_depth.npy")],
         "mask": [x for x in filelist if x.endswith("_depth_mask.npy")],
     }
+
     df = pd.DataFrame(data)
 
     df = df.sample(frac=1, random_state=42)
-
 
     HEIGHT = 256
     WIDTH = 256
@@ -76,74 +74,50 @@ if __name__ == "__main__":
         plt.show()
 
 
-    visualize_samples = next(
-        iter(DataGenerator(data=df, batch_size=6, dim=(HEIGHT, WIDTH)))
-    )
-    visualize_depth_map(visualize_samples)
+    # visualize_samples = next(
+    #     iter(DataGenerator(data=df, batch_size=6, dim=(HEIGHT, WIDTH)))
+    # )
+    # # visualize_depth_map(visualize_samples)
 
 
 
-    depth_vis = np.flipud(visualize_samples[1][1].squeeze())  # target
-    img_vis = np.flipud(visualize_samples[0][1].squeeze())  # input
+    # depth_vis = np.flipud(visualize_samples[1][1].squeeze())  # target
+    # img_vis = np.flipud(visualize_samples[0][1].squeeze())  # input
 
-    fig = plt.figure(figsize=(15, 10))
-    ax = plt.axes(projection="3d")
+    # fig = plt.figure(figsize=(15, 10))
+    # ax = plt.axes(projection="3d")
 
-    STEP = 3
-    for x in range(0, img_vis.shape[0], STEP):
-        for y in range(0, img_vis.shape[1], STEP):
-            ax.scatter(
-                [depth_vis[x, y]] * 3,
-                [y] * 3,
-                [x] * 3,
-                c=tuple(img_vis[x, y, :3] / 255),
-                s=3,
-            )
-        ax.view_init(45, 135)
+    # STEP = 3
+    # for x in range(0, img_vis.shape[0], STEP):
+    #     for y in range(0, img_vis.shape[1], STEP):
+    #         ax.scatter(
+    #             [depth_vis[x, y]] * 3,
+    #             [y] * 3,
+    #             [x] * 3,
+    #             c=tuple(img_vis[x, y, :3] / 255),
+    #             s=3,
+    #         )
+    #     ax.view_init(45, 135)
 
 
-    optimizer = tf.keras.optimizers.Adam(
-        learning_rate=LR,
-        amsgrad=False,
-    )
+    optimizer = tf.keras.optimizers.Adam(learning_rate=LR, amsgrad=False)
     model = DepthEstimationModel(width=WIDTH)
+
     # Define the loss function
-    cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction="none"
-    )
+    cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction="none")
+
     # Compile the model
     model.compile(optimizer, loss=cross_entropy)
 
+    model.build(input_shape=(None, HEIGHT, WIDTH, 3))
 
-    train_loader = DataGenerator(
-        data=df[:260].reset_index(drop="true"), batch_size=BATCH_SIZE, dim=(HEIGHT, WIDTH)
-    )
-    validation_loader = DataGenerator(
-        data=df[260:].reset_index(drop="true"), batch_size=BATCH_SIZE, dim=(HEIGHT, WIDTH)
-    )
+    model.summary()
+
+    train_loader = DataGenerator(data=df.reset_index(drop="true"), batch_size=BATCH_SIZE, dim=(HEIGHT, WIDTH))
+    validation_loader = DataGenerator(data=df[:250].reset_index(drop="true"), batch_size=BATCH_SIZE, dim=(HEIGHT, WIDTH))
+   
     model.fit(
         train_loader,
         epochs=EPOCHS,
         validation_data=validation_loader,
     )
-
-    model.summary()
-
-
-    test_loader = next(
-        iter(
-            DataGenerator(
-                data=df[265:].reset_index(drop="true"), batch_size=6, dim=(HEIGHT, WIDTH)
-            )
-        )
-    )
-    visualize_depth_map(test_loader, test=True, model=model)
-
-    test_loader = next(
-        iter(
-            DataGenerator(
-                data=df[300:].reset_index(drop="true"), batch_size=6, dim=(HEIGHT, WIDTH)
-            )
-        )
-    )
-    visualize_depth_map(test_loader, test=True, model=model)
